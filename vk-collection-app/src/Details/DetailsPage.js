c8c214eb834724d63e12933aca7f8c7f2f22bd70
ANGEL <atabakov99@abv.bg>
2023-06-04 13:13:57 +0300

    Added loader

--- vk-collection-app/src/Details/DetailsPage.js
+++ vk-collection-app/src/Details/DetailsPage.js
@@ -2,23 +2,34 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import * as productsService from '../services/productsService';
 import styles from './Details.module.css'
+import { ThreeDots } from 'react-loader-spinner';
 
 export const DetailsPage = () => {
   const { productId } = useParams();
 
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const productInfo = await productsService.getOneProduct(productId);
-      console.log(productInfo);
       setSelectedProduct(productInfo)
+      setIsLoading(false);
     }
     fetchData()
   }, [productId])
 
   return (
     <>
+      {isLoading && <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#4fa94d"
+        ariaLabel="three-dots-loading"
+        visible={true}
+      />}
       {selectedProduct && (
         <article className={styles['details-article']}>
           <section className={`${styles['details-section']} ${styles['container']}`}>
--- vk-collection-app/src/components/Product/ProductList.js
+++ vk-collection-app/src/components/Product/ProductList.js
@@ -1,12 +1,9 @@
-import React from 'react'
 import Product from './Product'
 import styles from './Products.module.css'
 
 function ProductList({
     products,
 }) {
-
-
     return (
         <div className={styles['product-container']}>
             <article className={styles['products']}>
